be7991d1d0b6de53f5a0270760e62f1f67989314
andrey.orlov.od@gmail.com <andrey.orlov.od@gmail.com>
2021-03-31 15:17:01 +0300

    added dispenser schemas

--- packages/oci-schemas/index.js
+++ packages/oci-schemas/index.js
@@ -1,3 +1,7 @@
 module.exports = {
+DSCSADispenserATPCredential: require('./schemas/DSCSADispenserATPCredential.json'),
+DSCSAManufacturerATPCredential: require('./schemas/DSCSAManufacturerATPCredential.json'),
+DSCSAWholesalerATPCredential: require('./schemas/DSCSAWholesalerATPCredential.json'),
 IdentityCredential: require('./schemas/IdentityCredential.json'),
+PostalAddress: require('./schemas/PostalAddress.json'),
 };
--- packages/oci-schemas/typings.d.ts
+++ packages/oci-schemas/typings.d.ts
@@ -1,4 +1,8 @@
 
 export default {
+DSCSADispenserATPCredential: require('./schemas/DSCSADispenserATPCredential.json'),
+DSCSAManufacturerATPCredential: require('./schemas/DSCSAManufacturerATPCredential.json'),
+DSCSAWholesalerATPCredential: require('./schemas/DSCSAWholesalerATPCredential.json'),
 IdentityCredential: require('./schemas/IdentityCredential.json'),
+PostalAddress: require('./schemas/PostalAddress.json'),
 }
